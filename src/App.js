34ee39bb2a80b5f6be0fab5dedb73f41cdc220d0
Yana Hristova <yana.hristova.work@gmail.com>
2025-08-17 21:57:22 +0300

    Replacing Step input with slider and adding a reset btn

--- src/App.js
+++ src/App.js
@@ -10,22 +10,34 @@ export default function App() {
 }
 
 function Counter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(1);
+  const [step, setStep] = useState(0);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
 
   const date = new Date();
   date.setDate(date.getDate() + count);
 
   return (
     <>
-      <div className="btns-holder">
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
-      </div>
+      <input
+        type="range"
+        min="0"
+        max="10"
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value))}
+      />
+      <span>Step: {step}</span>
       <div className="btns-holder">
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
 
@@ -39,6 +51,12 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </>
   );
 }
